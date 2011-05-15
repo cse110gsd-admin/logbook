5933b5b80d342e73d550d97660fe732b7f7568d6
Jake Rabinowitz <jrabinow@ucsd.edu>
2011-05-15 13:15:21 -0700

    Decided not to add admins...  (undid last commit)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user, :only => :destroy
   # "Edit" and "Account Management" are the same.
 
 
@@ -100,10 +99,6 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
-    end
-
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
     end
 
 
